f373375b8c3396a15799a9bfc6f218b2a9d389aa
matteotaf <tafuromatteo00@gmail.com>
2023-01-31 12:39:42 +0100

    Fix data path

--- openimages/faster_rcnn_r50_fpn_32x2_1x_openimages_challenge.py
+++ openimages/faster_rcnn_r50_fpn_32x2_1x_openimages_challenge.py
@@ -1,7 +1,7 @@
 _base_ = ['faster_rcnn_r50_fpn_32x2_1x_openimages.py']
 
 dataset_type = 'CocoDataset'
-data_root = '../../var/scratch/mtafuro/'
+data_root = '/var/scratch/mtafuro/'
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
